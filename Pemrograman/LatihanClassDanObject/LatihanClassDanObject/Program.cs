b091304e5437ea4b2462cc77f2eb9f674c273f9a
fathanNova <novaaroyan13@gmail.com>
2025-05-26 21:54:56 +0700

    first

---
+++ Pemrograman/LatihanClassDanObject/LatihanClassDanObject/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanClassDanObject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Mahasiswa mhs1 = new Mahasiswa();
+            Mahasiswa mhs2 = new Mahasiswa();
+
+            mhs1.Nama = "Fathan";
+            mhs1.Nim = "224";
+            mhs1.Ipk = 3.8f;
+
+            mhs2.Nama = "Nova";
+            mhs2.Nim = "225";
+            mhs2.Ipk = 3.8f;
+
+            mhs1.Registrasi();
+            mhs1.isiKrs();
+
+            mhs2.Registrasi();
+            mhs2.isiKrs();
+
+            Console.ReadKey();
+        }
+    }
+}
