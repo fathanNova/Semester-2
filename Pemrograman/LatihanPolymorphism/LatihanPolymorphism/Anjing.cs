9385a6c349e29b93c2bd6eb0904053483d844238
fathanNova <novaaroyan13@gmail.com>
2025-06-09 16:18:39 +0700

    first

---
+++ Pemrograman/LatihanPolymorphism/LatihanPolymorphism/Anjing.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanPolymorphism
+{
+    public class Anjing : Hewan
+    {
+        public override void Suara()
+        {
+            Console.WriteLine("Anjing Bernama {0} Bersuara: Guk Guk", Name);
+        }
+    }
+}
---
+++ Pemrograman/LatihanPolymorphism/LatihanPolymorphism/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanPolymorphism
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Hewan hewan = new Hewan();
+            //hewan.Suara();
+
+            Kucing kucing = new Kucing();
+            hewan = kucing;
+            hewan.Name = "Kitty";
+            hewan.Suara();
+
+            Anjing anjing = new Anjing();
+            hewan = anjing;
+            hewan.Name = "Black";
+            hewan.Suara();
+
+            Console.ReadKey();
+        }
+    }
+}
